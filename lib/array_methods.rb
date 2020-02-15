f3b605247398ee834162391444bcefaf1db855ef
Ruth Obe <ruthobey@gmail.com>
2020-02-15 20:05:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -32,8 +32,8 @@ def find_max_value(array)
   
 end
 
-# def find_min_value(array)
-#   # Add your solution here
-# end
+def find_min_value(array)
+  # Add your solution here
+end
 
 # find_max_value([1,2,1,3,4,3,5,4,3,2,1])
