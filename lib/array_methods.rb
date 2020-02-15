92fb8d32d094b740f359edaccf39fe941bd59e9b
Ruth Obe <ruthobey@gmail.com>
2020-02-15 19:35:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,10 +15,13 @@ def find_element_index(array, value_to_find)
      
 end
 
-def find_max_value(array)
-  # Add your solution here
-end
+# def find_max_value(array)
+#   # Add your solution here
+# end
 
-def find_min_value(array)
-  # Add your solution here
-end
+# def find_min_value(array)
+#   # Add your solution here
+# end
+
+
+find_element_index([1,2,3,4], 4)
